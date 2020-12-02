c1d6a574586ab598fd88ca7652160af5f473ba97
ZhangAoHan <1647110671@qq.com>
2020-12-02 16:46:15 +0800

    。

--- C语言练习代码/Strlen模拟/test.c
+++ C语言练习代码/Strlen模拟/test.c
@@ -1,26 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
+//strlen 模拟
 #include <stdio.h>
 
-int Str_len(char *str); //strlen
+int Str_len(char *str,int sit); //strlen
 int main()
 {
-	char str[5];
+	char str[100];   //输入字符串长度必须小于100
 	int len = 0;
 	scanf("%s",str); //取出字符串首地址
-	len = Str_len(str);
-	printf("%d",len);
+	len = Str_len(str,1);
+	printf("递归求解结果为： %d\n",len);
+	len = Str_len(str, 0);
+	printf("循环求解结果为： %d\n", len);
 	return 0;
 }
 
-int Str_len(char* strs)
+int Str_len(char* strs,int sit)
 {
-	if(*strs == '\0')
-	{ 
-		return 0;
-	}
-	else
+	if (sit)  //递归求解
 	{
-		return 1 + Str_len(strs +1);
+		if (*strs == '\0')
+		{
+			return 0;
+		}
+		else
+		{
+			return 1 + Str_len(strs + 1,1);
+		}
 	}
+	else  //非递归求解
+	{
+		int len = 0;
+		while (*strs != '\0')
+		{
+			len++;
+			strs++;
+		}
+		return len;
+	}
+
 }
+
+
